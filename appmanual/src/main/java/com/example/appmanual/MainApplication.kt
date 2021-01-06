cb6e5c6b0b532038a6385ca5c58654123b79a404
elye <elye.project@gmail.com>
2021-01-06 21:59:55 +1100

    Remove myBase from auto startup only

--- appmanual/src/main/java/com/example/appmanual/MainApplication.kt
+++ appmanual/src/main/java/com/example/appmanual/MainApplication.kt
@@ -4,7 +4,6 @@ import android.app.Application
 import android.util.Log
 import androidx.startup.AppInitializer
 import com.example.mybase.MyBaseInitializer
-import com.example.mylib.MyLibInitializer
 
 class MainApplication: Application() {
     override fun onCreate() {
@@ -12,6 +11,5 @@ class MainApplication: Application() {
         Log.d("TrackingLib", "MainApplication Started")
         val initializer = AppInitializer.getInstance(this)
         initializer.initializeComponent(MyBaseInitializer::class.java)
-        initializer.initializeComponent(MyLibInitializer::class.java)
     }
 }
